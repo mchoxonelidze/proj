d511b58daf4f685c90346971bf44691561216db5
unknown <mariamchoxonelidze11@gmail.com>
2019-12-03 22:47:59 +0800

    aa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ function Home() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ad nihil commodi laudantium voluptatem nisi aut vel excepturi accusamus! Mollitia sapiente praesentium unde eum amet at ea natus doloribus expedita.
 
             </p>
+            <h2>ssssssssssaaaaaaaaaa</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ad nihil commodi laudantium voluptatem nisi aut vel excepturi accusamus! Mollitia sapiente praesentium unde eum amet at ea natus doloribus expedita.
                 
                 </p>
